5669f6392e09e4c11a9ce78b0cc5436f5b7f4d22
Todd Sayre <captain.friendly@gmail.com>
2024-07-11 16:15:34 -0400

    Par2 work

--- cbz_from_folder.py
+++ cbz_from_folder.py
@@ -7,13 +7,13 @@
 
 # MARK: Imports
 
-import json
 import os
 import shutil
 import subprocess
 import sys
 import zipfile
 from pathlib import Path
+from pprint import pprint
 from zipfile import is_zipfile
 
 from macos_tags import Color, Tag
@@ -50,50 +50,51 @@ for arg in args:
     if dst_cbz.exists():
         add_tag(T_collision, file=str(src_dir))
 
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # MARK: Parity Files
-
-    # Add in the parity files
-    # par2 c -t0 -r10 -d./ -- Recovery.par2
-    # set -l recovery_path "$temp_dir_recovery/Recovery.par2"
-    # set -l file_list (find "$temp_dir_recovery" -type f | gshuf)
-    # par2 c -B"$temp_dir_recovery" -r10 -q "$recovery_path" $file_list
-    # P_recovery = os.path.join(src_dir, "Recovery.par2")
-    # file_list = []
-    # print(P_recovery)
-    # for start, _, files in os.walk(src_dir):
-    #     for file in files:
-    #         file = Path(file)
-    #         if file.name in [".DS_Store"]:
-    #             continue
-    #         file_list.append(os.path.join(start, file))
-    # print(file_list)
-    # print(" ".join([f'"{json.dumps(fn)}"' for fn in file_list]))
-    # print(" ".join([json.dumps(fn) for fn in file_list]))
-    # os.system(
-    #     f"par2 c -B\"{src}\" -r10 -q \"{recovery_path}\" {' '.join(file_list)}"
+    # Delete any existing Recovery (par2) records
+    # subprocess.run(
+    #     [
+    #         "find",
+    #         src_dir,
+    #         "-type",
+    #         "f",
+    #         "-name",
+    #         "*.par2",
+    #         "-delete",
+    #     ]
     # )
-    # x = [
-    #     "par2",
-    #     "create",
-    #     f'-B"{src_dir}"',
-    #     "-r10",
-    #     # "-q", # Quiet
-    #     "-v",  # Verbose
-    #     # "Recovery.par2",
-    #     f'{os.path.join(src_dir, "Recovery.par2")}',
-    #     "--",
-    #     # "(find . -type f | gshuf)",
-    # ]
-    # x.extend([f'"{fn}"' for fn in file_list])
-    # print(" ".join(x))
-    # subprocess.Popen(x)
-    # subprocess.check_call(x)
-    # os.chdir(src_dir)
-    # print(subprocess.run("pwd"))
-    # print(subprocess.run(["par2", "-VV"]))
-    # subprocess.run(" ".join(x))
-    # subprocess.run(x)
-    # continue
+
+    # Create Recovery (par2) records
+    # found_files = subprocess.run(
+    #     [
+    #         "find",
+    #         src_dir,
+    #         "-type",
+    #         "f",
+    #         "-not",
+    #         "-name",
+    #         ".DS_Store",
+    #     ],
+    #     capture_output=True,
+    #     text=True,
+    # )
+
+    # subprocess.run(
+    #     [
+    #         "par2",
+    #         "create",
+    #         "-aRecovery",
+    #         "-q",
+    #         "-r10",
+    #         "--",
+    #         *found_files.stdout.split("\n"),
+    #     ],
+    #     cwd=src_dir,
+    #     capture_output=True,
+    #     text=True,
+    # )
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     with zipfile.ZipFile(
         dst_cbz,
