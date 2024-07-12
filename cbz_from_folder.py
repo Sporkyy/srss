c2f94eb14606673a5aec7025406c2ec8f2ce2f68
Todd Sayre <captain.friendly@gmail.com>
2024-07-12 09:53:27 -0400

    Add par2 back in and use patoolib

--- cbz_from_folder.py
+++ cbz_from_folder.py
@@ -3,19 +3,21 @@
 #
 # ## External Dependencies
 #   * https://pypi.org/project/macos-tags/
+#   * https://pypi.org/patool/
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 # MARK: Imports
 
 import os
 import shutil
+import subprocess
 import sys
-import zipfile
 from pathlib import Path
 from zipfile import is_zipfile
 
 from macos_tags import Color, Tag
 from macos_tags import add as add_tag
+from patoolib import create_archive
 
 # import subprocess
 # from pprint import pprint
@@ -26,10 +28,12 @@ T_ok = Tag(
     name="Comic is ok",
     color=Color.GREEN,
 )
+
 T_bad = Tag(
     name="Comic is bad",
     color=Color.RED,
 )
+
 T_collision = Tag(
     name="File name collision",
     color=Color.YELLOW,
@@ -41,80 +45,75 @@ args = sys.argv[1:]
 for arg in args:
 
     src_dir = Path(arg)
-
-    # Ensure a directory
-    if not src_dir.is_dir():
-        continue
-
     dst_cbz = src_dir.with_suffix(".cbz")
 
+    # Ensure a directory
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    if not src_dir.is_dir():
+        continue
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
     # Ensure no collision
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     if dst_cbz.exists():
         add_tag(T_collision, file=str(src_dir))
-
+        continue
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    # MARK: Parity Files
+
+    # MARK: Add Parity Files
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     # Delete any existing Recovery (par2) records
-    # subprocess.run(
-    #     [
-    #         "find",
-    #         src_dir,
-    #         "-type",
-    #         "f",
-    #         "-name",
-    #         "*.par2",
-    #         "-delete",
-    #     ]
-    # )
+    subprocess.run(
+        [
+            "find",
+            src_dir,
+            "-type",
+            "f",
+            "-name",
+            "*.par2",
+            "-delete",
+        ]
+    )
+
+    # Find files to protect
+    find_output = subprocess.run(
+        [
+            "find",
+            src_dir,
+            "-type",
+            "f",
+        ],
+        capture_output=True,
+        text=True,
+    )
 
     # Create Recovery (par2) records
-    # found_files = subprocess.run(
-    #     [
-    #         "find",
-    #         src_dir,
-    #         "-type",
-    #         "f",
-    #         "-not",
-    #         "-name",
-    #         ".DS_Store",
-    #     ],
-    #     capture_output=True,
-    #     text=True,
-    # )
-
-    # subprocess.run(
-    #     [
-    #         "par2",
-    #         "create",
-    #         "-aRecovery",
-    #         "-q",
-    #         "-r10",
-    #         "--",
-    #         *found_files.stdout.split("\n"),
-    #     ],
-    #     cwd=src_dir,
-    #     capture_output=True,
-    #     text=True,
-    # )
+    part2_output = subprocess.run(
+        [
+            "par2",
+            "create",
+            "-aRecovery",
+            "-q",
+            "-r10",
+            "--",
+            *find_output.stdout.split("\n"),
+        ],
+        cwd=src_dir,
+    )
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    with zipfile.ZipFile(
-        dst_cbz,
-        mode="w",
-        compression=zipfile.ZIP_STORED,
-    ) as archive:
-        for start, _, files in os.walk(src_dir):
-            for file in files:
-                file = Path(file)
-                if file.name in [".DS_Store"]:
-                    continue
-                file_path = os.path.join(start, file)
-                arcname = os.path.relpath(file_path, src_dir)
-                archive.write(file_path, arcname)
-
-    if is_zipfile(dst_cbz):
-        add_tag(T_ok, file=str(dst_cbz))
-        shutil.rmtree(src_dir)
-    else:
-        print(f"Invalid {dst_cbz}")
+    try:
+        os.chdir(src_dir.parent)
+        create_archive(str(dst_cbz.name), [str(src_dir.name)])
+        if is_zipfile(dst_cbz):
+            print(f"✅ CBZ OK {dst_cbz}")
+            add_tag(T_ok, file=str(dst_cbz))
+            shutil.rmtree(src_dir)
+        else:
+            print(f"🛑 CBZ Invalid {dst_cbz}")
+            add_tag(T_bad, file=str(dst_cbz))
+    except Exception as e:
+        print(f"❗️ Error: {e}")
         add_tag(T_bad, file=str(dst_cbz))
+        if dst_cbz.exists():
+            os.remove(dst_cbz)
