707543c84d45385fa9a16eeae20bd3abd4385041
Todd Sayre <captain.friendly@gmail.com>
2024-09-25 17:08:03 -0400

    ImageMagick can't be found try again later

--- image_tag_resolution.py
+++ image_tag_resolution.py
@@ -1,5 +1,5 @@
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-# Image: Tag Resolution
+# Image: Tag Info
 #
 # Resolutions are tagged by the nearest smallest "named" resolution.
 #
@@ -25,6 +25,7 @@ import sys
 
 # MARK: Imports
 from ast import List
+from math import e
 from os import remove
 from pathlib import Path
 from re import T
@@ -34,23 +35,27 @@ from macos_tags import Color, Tag
 from macos_tags import add as add_tag
 from macos_tags import get_all as get_all_tags
 from macos_tags import remove as remove_tag
-from PIL import Image
+from PIL import Image as ImageP
+from wand.image import Image as ImageW
 
-res_tags = {
+# MARK: Tags
+
+T_CORRUPT = Tag(name="Corrupt", color=Color.RED)
+
+RES_TAGS = {
     # For tagging purposes the `(width, height)` tuple is the minimum resolution
     Tag(name="8K", color=Color.BLUE): (7680, 4320),
     Tag(name="6K", color=Color.BLUE): (6144, 3456),
     Tag(name="5K", color=Color.GREEN): (5120, 2880),
     Tag(name="4K", color=Color.GREEN): (3840, 2160),
     Tag(name="1080p", color=Color.YELLOW): (1920, 1080),
-    Tag(name="Tiny", color=Color.RED): (0, 0),
 }
 
-T_portrait = Tag(name="Portrait", color=Color.PURPLE)
-
-T_landscape = Tag(name="Landscape", color=Color.PURPLE)
-
-T_square = Tag(name="Square", color=Color.PURPLE)
+ORIENTATION_TAGS = {
+    Tag(name="Portrait", color=Color.PURPLE): lambda x: x < 1,
+    Tag(name="Square", color=Color.PURPLE): lambda x: 1 == x,
+    Tag(name="Landscape", color=Color.PURPLE): lambda x: 1 < x,
+}
 
 
 # MARK: Functions
@@ -64,33 +69,64 @@ def is_image(file: Path) -> bool:
         return False
     fn_suffix = file.suffix.lower()
     image_suffixes = [".bmp", ".gif", ".jpeg", ".jpg", ".png", ".tiff"]
-    return fn_suffix in image_suffixes
+    if fn_suffix not in image_suffixes:
+        return False
+    try:
+        pil_check(file)
+        magick_check(file)
+    except Exception as e:
+        print(f"🚫 {file.name} {e}")
+        add_tag(T_CORRUPT, file=arg)
+        return False
+    return True
 
 
 # This should work since `dict`s have a guaranteed order in Python 3.7+
 def get_tags_by_dimensions(width: int, height: int) -> list:
     tags = []
-    if width < height:
-        tags.append(T_portrait)
-    elif height < width:
-        tags.append(T_landscape)
-    else:
-        tags.append(T_square)
-    for tag, (min_width, min_height) in res_tags.items():
+    for tag, orientation in ORIENTATION_TAGS.items():
+        if orientation(width / height):
+            tags.append(tag)
+            break
+    for tag, (min_width, min_height) in RES_TAGS.items():
         if min_width <= width and min_height <= height:
             tags.append(tag)
+            break
     return tags
 
 
-def pil_check(filename):
-    img = Image.open(filename)  # open the image file
+def remove_info_tags(file: Path):
+    for tag in get_all_tags(file.absolute().as_posix()):
+        if tag in RES_TAGS.keys():
+            remove_tag(tag, file=file.absolute().as_posix())
+        if tag in ORIENTATION_TAGS.keys():
+            remove_tag(tag, file=file.absolute().as_posix())
+        if T_CORRUPT == tag:
+            remove_tag(tag, file=file.absolute().as_posix())
+
+
+# https://github.com/ftarlao/check-media-integrity/blob/master/check_mi.py
+def pil_check(filename: Path):
+    img = ImageP.open(filename)  # open the image file
     img.verify()  # verify that it is a good image, without decoding it.. quite fast
     img.close()
 
     # Image manipulation is mandatory to detect few defects
-    img = Image.open(filename)  # open the image file
-    img.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    img = ImageP.open(filename)  # open the image file
+    img.transpose(ImageP.Transpose.FLIP_LEFT_RIGHT)
     img.close()
+
+
+# https://github.com/ftarlao/check-media-integrity/blob/master/check_mi.py
+def magick_check(filename: Path, flip=True):
+    # very useful for xcf, psd and aslo supports pdf
+    img = ImageW(filename=filename)
+    if flip:
+        temp = img.flip
+    else:
+        temp = img.make_blob(format="bmp")
+    img.close()
+    return temp
 
 
 args = sys.argv[1:]
@@ -104,23 +140,12 @@ for arg in args:
     if not is_image(P_arg):
         continue
 
-    try:
-        pil_check(P_arg)
-    except Exception as e:
-        print(f"🚫 {P_arg.name} {e}")
-        continue
+    # Remove any existing resolution tags
+    remove_info_tags(P_arg)
 
     # Get the image resolution
-    with Image.open(P_arg) as img:
+    with ImageP.open(P_arg) as img:
         width, height = img.size
-
-    # Remove any existing resolution tags
-    remove_tag(T_portrait, file=arg)
-    remove_tag(T_landscape, file=arg)
-    remove_tag(T_square, file=arg)
-    for tag in get_all_tags(arg):
-        if tag.name in res_tags.keys():
-            remove_tag(tag, file=arg)
 
     # Get+set the tags
     for tag in get_tags_by_dimensions(width, height):
