f2042044a20887e097045bde3a36845e3b7de088
Todd Sayre <captain.friendly@gmail.com>
2025-01-14 17:02:03 -0500

    Fix-ups

--- video-rename-with-resolution.py
+++ video-rename-with-resolution.py
@@ -9,8 +9,10 @@
 
 from os import environ, pathsep
 from pathlib import Path
+from re import IGNORECASE as re_IGNORECASE
 from re import compile as re_compile
 from re import search as re_search
+from re import sub as re_sub
 from sys import argv
 
 from imageio_ffmpeg import read_frames
@@ -49,31 +51,64 @@ MOVIE_SUFFIXES = [
 args = argv[1:]
 for arg in args:
 
-    path = Path(arg).resolve()
+    src = Path(arg).resolve()
 
     # ♻️ Skip if the path is not a file
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    if not path.is_file():
-        print(f"🛑 {path.name} 👉 not a file")
+    if not src.is_file():
+        print(f"🛑 {src.name} 👉 not a file")
         continue
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     # ♻️ Skip if the path does not have a recognized suffix
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    if not path.suffix.lower() in MOVIE_SUFFIXES:
-        print(f"🛑 {path.suffix} 👉 not a movie suffix")
+    if not src.suffix.lower() in MOVIE_SUFFIXES:
+        print(f"🛑 {src.suffix} 👉 not a movie suffix")
         continue
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    # Normalize dimensions (<w>x<h>) in the file name to resolution (<h>p)
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    pattern_dimensions = re_compile(r"\b(\d{3,4})\s?[xX]\s?(\d{3,4})\b")
+    matches = re_search(pattern_dimensions, src.stem)
+    if matches:
+        print(f"📏 {src.name} (name) 👉 {matches.group(1)}")
+        fn_height = matches.group(2)
+        dst = src.with_stem(re_sub(pattern_dimensions, f"{fn_height}p", src.stem))
+        print(f"♻️ Renaming {src} 👉 {dst}")
+        src.rename(dst)
+        src = dst
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    # Get the resolution from the file name
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    pattern_resolution = re_compile(r"\b(\d{3,4}[pP])\b")
+    fn_resolution = None
+    matches = re_search(pattern_resolution, src.stem)
+    if matches:
+        fn_resolution = matches.group(1)
+        print(f"📏 {src.name} (name) 👉 {fn_resolution}")
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    # If the "p" is capitalized, make it lowercase
+    # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    if fn_resolution:
+        if "P" in fn_resolution:
+            fn_resolution = fn_resolution.lower()
+            dst = src.with_stem(re_sub(pattern_resolution, fn_resolution, src.stem))
+            print(f"♻️ Lowering case {src} 👉 {dst}")
+            dst = src.rename(dst)
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     # (Try to) get the metadata or skip+tag if the video is corrupt
     # And if the video is corrupt, rename it to indicate that
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     try:
-        reader = read_frames(path)
+        reader = read_frames(src)
         meta = reader.__next__()
     except Exception as e:
-        print(f"🚫 {path.name} 👉 corrupt")
-        path.rename(path.with_stem(f"_CORRUPT_ {path.stem}"))
+        print(f"🚫 {src.name} 👉 corrupt")
+        src.rename(src.with_stem(f"_CORRUPT_ {src.stem}"))
         print(e)
         continue
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -81,18 +116,27 @@ for arg in args:
     # Get the video resolution
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     vid_width, vid_height = meta["size"]
-    print(f"📏 {path.name} 👉 {vid_width}x{vid_height}")
-    pattern_resolution = re_compile(rf"\b{vid_height}[pP]\b")
+    print(f"📏 {src.name} (meta) 👉 {vid_width}x{vid_height}")
+    vid_resolution = f"{vid_height}p"
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    # Skip if the resolution is already in the file name
+    # Skip if the resolution is already in the file name or replace it if it's wrong
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    if re_search(pattern_resolution, path.name):
-        print(f"🛑 {path.name} 👉 already has resolution")
-        continue
+    if fn_resolution:
+        if fn_resolution == vid_resolution:
+            print(f"🚫 {src.name} 👉 already has correct resolution")
+            continue
+        else:
+            dst = src.with_stem(re_sub(pattern_resolution, vid_resolution, src.stem))
+            print(f"♻️ {src} 👉 {dst}")
+            src.rename(dst)
+            continue
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-    # Rename the file to append the resolution
+    # Add the resolution to the file name if it is missing
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-    path.rename(path.with_stem(f"{path.stem} [{vid_height}p]"))
+    if not fn_resolution:
+        dst = src.with_stem(f"{src.stem} [{vid_resolution}]")
+        print(f"♻️ {src} 👉 {dst}")
+        src.rename(dst)
     # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
